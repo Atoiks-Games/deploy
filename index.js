2ded704e74cb480047db4a506b805a38181c16ba
Plankp T <plankp@outlook.com>
2016-03-25 14:18:54 -0400

    Added nightmare mode

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ app.set('port', (process.env.PORT || 5000))
 
 app.use(express.static(__dirname))
 
-app.get('/die-or-rage', function (req, res) {
-  res.render(__dirname + '/die-or-rage/index.html')
-})
+app.get('/die-or-rage',
+  (req, res) => res.render(__dirname + '/die-or-rage/index.html'))
 
-app.get('/', function (req, res) {
-  res.redirect('http://prototype-games.github.io/')
-})
+app.get('/die-or-rage-nm',
+  (req, res) => res.render(__dirname + '/die-or-rage-nm/index.html'))
 
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port ' + app.get('port'))
-})
+app.get('/',
+  (req, res) => res.redirect('http://prototype-games.github.io/'))
+
+app.listen(app.get('port'),
+  () => console.log('Node app is running on port ' + app.get('port')))
