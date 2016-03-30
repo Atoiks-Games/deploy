e1c758f1e0d03400ade4546a62382695058783ef
Plankp T <plankp@outlook.com>
2016-03-30 10:39:06 -0400

    Added game Jarg

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.get('/die-or-rage',
 app.get('/die-or-rage-nm',
   (req, res) => res.render(__dirname + '/die-or-rage-nm/index.html'))
 
+app.get('/Jarg',
+  (req, res) => res.render(__dirname + '/Jarg/index.html'))
+
 app.get('/',
   (req, res) => res.redirect('http://atoiks-games.github.io'))
 
