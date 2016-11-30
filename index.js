055ad5ae86e9c385a0e00013563272ca07bde038
Plankp T <plankp@outlook.com>
2016-11-30 17:08:53 -0500

    Fixing internal server error

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-app.get('/die-or-rage',
-	(req, res) => res.render(__dirname + '/die-or-rage/index.html'))
+app.get('/die-or-rage', function (req, res) {
+	res.sendFile(__dirname + '/die-or-rage/index.html')
+})
 
-app.get('/die-or-rage-nm',
-	(req, res) => res.render(__dirname + '/die-or-rage-nm/index.html'))
+app.get('/die-or-rage-nm', function (req, res) {
+	res.sendFile(__dirname + '/die-or-rage-nm/index.html')
+})
 
+/*
 app.get('/jarg-1',
 	(req, res) => res.render(__dirname + '/jarg-1/index.html'))
 
-/*
 app.get('/jarg-2',
 	(req, res) => res.render(__dirname + '/jarg-2/index.html'))
-*/
 
 app.get('/snark',
 	(req, res) => res.render(__dirname + '/snark/index.html'))
@@ -29,6 +30,8 @@ app.get('/',
 app.use((req, res) => res.status(404).send('<em>The page you are trying to navigate to cannot be found! Click <a href="/">here</a> to return to homepage.</em>'));
 
 app.use((req, res) => res.status(500).send('<em>Something went wrong! Click <a href="/">here</a> to return to homepage.</em>'));
+*/
 
-app.listen(app.get('port'),
-	() => console.log('Node app is running on port ' + app.get('port')))
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port ' + app.get('port'))
+})
