baffdc7ef57fd2bef07570569d117b1676cf23d8
PerenDarksbane <ajbimm@gmail.com>
2018-05-25 15:11:41 -0400

    taptaptap
    taptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptaptape

---
+++ public/taptaptap/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1527275375,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/leftbtn-sheet0.png",
+		"images/type1-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/type2-sheet0.png",
+		"images/spawn-sheet0.png",
+		"images/bottomborder-sheet0.png",
+		"media/tap3.m4a",
+		"media/tap3.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
